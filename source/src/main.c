545175d500363d3637d955ff071db57d97e9f431
AlanDeivite <allan.deivite@gmail.com>
2016-10-13 10:02:44 -0300

    leitura iterativa para o dataset UCMerced

--- source/src/main.c
+++ source/src/main.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <sys/types.h>
 #include "../include/image.h"
 #include "../include/vgg19.h"
 #include "../include/cnn_extraction.h"
@@ -20,7 +22,7 @@ int main()
     double featureVector[4096];
     /// reads the parameters of VGG 19
     readFile();
-    /// reads iteratively all the images in the dataset
+    /// reads iteratively all the images in the coffee dataset
     for(it = 1; it <= K_FOLD; it++)
     {
         sprintf(dataset_dir, "%s/coffee/images/fold%d.txt", DATASET_DIR, it);
@@ -46,5 +48,36 @@ int main()
         }
         fclose(picture_file);
     }
+    /// reads iteratively all the images in the UCMerced dataset
+    struct dirent *file;
+    struct dirent *folder;
+    DIR* dir_path = opendir("../dataset/UCMerced/images");
+    if (dir_path != NULL)
+    {
+        while( (folder = readdir (dir_path)) )
+        {
+            if(!strncmp(folder->d_name, ".", 1))
+                continue;
+            sprintf(dataset_dir, "%s/UCMerced/images/%s", DATASET_DIR, folder->d_name);
+            DIR* img_path = opendir(dataset_dir);
+            while( (file = readdir (img_path)) )
+            {
+                if(!strncmp(file->d_name, ".", 1))
+                    continue;
+                sprintf(image_path, "%s/%s", dataset_dir, file->d_name);
+                /// TODO create the forward method that will receive the path to each image in dataset and return a corresponding feature vector
+                //featureVector = forward(image_path);
+                /// saves the features vector in each specific folder
+                sprintf(feature_path, "%s/UCMerced/features/%s/features.txt", DATASET_DIR, folder->d_name);
+                FILE* feature_file = fopen(feature_path, "w");
+                //fwrite(featureVector, sizeof(double), 4096, feature_file);
+                fclose(feature_file);
+            }
+            (void) closedir(img_path);
+        }
+        (void) closedir(dir_path);
+    }
+    else
+        perror("Couldn't open the directory");
     return 0;
 }
